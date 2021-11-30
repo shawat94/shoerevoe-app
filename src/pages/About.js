658d192149f4cff0fec91b88ec44287b600b226e
shawat94 <shawa947@gmail.com>
2021-11-29 21:27:29 -0800

    About section

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,7 +7,7 @@ const About = () => {
       <div className='about-page'>
         <h1 className='page-title' align="center">About</h1>
         <p className='about-content' align="center">
-          ShoeRevoe is a running shoe review website run entirely by someone who is subpar at running and doesn't know much about running shoes. All content should be read as though the writer has absolutely no idea what they are talking about.
+          ShoeRevoe is a running shoe review website run entirely by someone who is subpar at running, doesn't even enjoy it that much, and doesn't know much about running shoes. All content should be read as though the writer has absolutely no idea what they are talking about.
         </p>
       </div>
     </div>
